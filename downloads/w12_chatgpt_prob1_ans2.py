d04b4cea50c3d2995bbeb756f3a8b9a732928e2d
mdecycu <mdecycuorg@gmail.com>
2023-11-30 13:08:13 +0800

    使用 ChatGPT 協助解決 w12 所面臨的問題一

---
+++ downloads/w12_chatgpt_prob1_ans2.py
@@ -0,0 +1,31 @@
+# 指定檔案路徑
+file_path = "2b_w12_seat.txt"
+
+# 打開檔案
+with open(file_path, "r", encoding="utf-8") as file:
+    # 逐行讀取檔案內容
+    lines = file.readlines()
+
+# 創建一個字典來存儲學號和座位號的對應關係
+seat_map = {}
+
+# 迭代每一行
+for line in lines:
+    # 以 tab 分割每一行的內容
+    elements = line.split("\t")
+
+    # 如果該行包含座位信息
+    if len(elements) == 2 and "(" in elements[1]:
+        # 提取學號和座位號
+        stud_num = elements[0].strip()
+        seat_info = elements[1].strip()
+
+        # 將學號和座位號添加到字典中
+        seat_map[stud_num] = seat_info
+
+# 將座位號按照列和行排序
+sorted_seats = sorted(seat_map.items(), key=lambda x: (int(x[1][1]), int(x[1][3])))
+
+# 顯示結果
+for stud_num, seat_info in sorted_seats:
+    print(f"座位號: {seat_info}, 學號: {stud_num}")
